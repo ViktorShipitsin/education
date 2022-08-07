a25a5cd3f98897fe8fef050303fe8742db9b255b
ViktorShipitsin <shipitsin.viktor.vladimirovych@yandex.ru>
2022-08-07 12:28:09 +0800

    Ex_003 complete.

--- HomeworksAndLessons/SeminarHomeworks/Seminar_001/Ex_003/Program.cs
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_001/Ex_003/Program.cs
@@ -1,2 +1,22 @@
 ﻿// Задача 4: Напишите программу, которая принимает на вход три числа и выдаёт максимальное из этих чисел.
-Console.WriteLine("Hello, World!");
+
+
+// Условно взятые числа.
+int a = 2;
+int b = 3;
+int c = 7;
+
+int max_value = a;
+string max = "a";
+
+if (a < b)
+    {max = "b";
+    max_value = b;}
+
+if (b < c)
+    {max = "c";
+    max_value = c;}
+
+Console.Write(max);
+Console.Write(", равное ");
+Console.Write(max_value);
