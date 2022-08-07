ec9c4601ef5d0182e426415c05b3bb96db1fc38c
ViktorShipitsin <shipitsin.viktor.vladimirovych@yandex.ru>
2022-08-07 12:08:22 +0800

    Ex_001 complete, and Ex_003 starting.

--- HomeworksAndLessons/SeminarHomeworks/Seminar_001/Ex_001/Program.cs
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_001/Ex_001/Program.cs
@@ -1,2 +1,10 @@
 ﻿// Задача 2: Напишите программу, которая на вход принимает два числа и выдаёт, какое число большее, а какое меньшее.
-Console.WriteLine("Hello, World!");
+
+//Условно взятые числа.
+int a = 5;
+int b = 7;
+
+if ( a > b )
+    Console.WriteLine("a > b");
+else
+    Console.WriteLine("b > a");
