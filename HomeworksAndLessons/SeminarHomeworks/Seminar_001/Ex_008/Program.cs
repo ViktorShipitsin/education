146ec450b767b77f2a4ca05b49cb91981b745282
ViktorShipitsin <shipitsin.viktor.vladimirovych@yandex.ru>
2022-08-07 14:24:36 +0800

    Ex_008 starting and complete.

---
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_001/Ex_008/Program.cs
@@ -0,0 +1,13 @@
+﻿// Задача 8: Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+
+// Условно взятое число.
+int N = 5;
+
+int count = N;
+int remain = 0;
+
+while (count > 1)
+    {remain = count % 2;
+        if (remain == 0)
+            Console.WriteLine(count);
+    count = count - 1;}
