d45eca9e2dced7408714f62c5a72d9a08b948816
ViktorShipitsin <shipitsin.viktor.vladimirovych@yandex.ru>
2022-08-08 14:30:17 +0800

    Start homework for in Seminar_002. Ex_010 started and completed.

---
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_002/Ex_010/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 10: Напишите программу, которая принимает на вход трёхзначное число и на выходе показывает вторую цифру этого числа.
+
+string string1 = "Взято случайное трёхзначное число: ";
+string string2 = "Второй цифрой числа является: ";
+
+int RandomValue = new Random().Next(100, 1000);
+int x = ( RandomValue - ( RandomValue / 100 * 100 ) ) / 10;
+
+Console.WriteLine(string1 + RandomValue);
+Console.WriteLine(string2 + x);
---
+++ Seminar_0002/Ex_011/Program.cs
@@ -0,0 +1,19 @@
+﻿// 11. Напишите программу, которая выводит случайное трёхзначное число и удаляет вторую цифру этого числа.
+
+int random(){
+    Random rnd = new Random();
+    int a = rnd.Next(100, 999);
+    return a;
+}
+
+int razdel (int a) {
+    int x = a%10;
+    int y = a/100;
+    return y*10+x;
+}
+
+void vivod (int a, int z) {
+    Console.WriteLine("случайное число " + a);
+    Console.WriteLine(z);
+}
+
