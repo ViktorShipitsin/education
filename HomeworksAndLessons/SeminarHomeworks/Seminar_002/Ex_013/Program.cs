8e59d1f34803916925286c66c0e2ec39655b1377
ViktorShipitsin <shipitsin.viktor.vladimirovych@yandex.ru>
2022-08-09 14:43:21 +0800

    Ex_013 complete.

---
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_002/Ex_013/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 13: Напишите программу, которая выводит третью цифру заданного числа или сообщает, что третьей цифры нет.
+
+int RandomValue = new Random().Next(1, 1000000);
+
+string strThirdDigit = RandomValue.ToString();
+
+Console.Write("Взятое число: ");
+Console.WriteLine(RandomValue);
+Console.Write("Третья цифра взятого числа: ");
+Console.WriteLine(strThirdDigit[2]);
