9c0701ee7c0fa26ee9bdbc1f775f69d9b3a2b1ce
ViktorShipitsin <shipitsin.viktor.vladimirovych@yandex.ru>
2022-08-10 15:48:47 +0800

    Some finally edits in Ex_011 and Ex_013. Ex_015 complete.

---
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_002/Ex_015/Program.cs
@@ -0,0 +1,10 @@
+﻿// Задача 15: Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
+
+void dayWrite()
+{
+    string[] daysArray = {"пн", "вт", "ср", "чт", "пт", "сб", "вс"};
+    int RandomValue = new Random().Next(0, 7);
+    Console.WriteLine(daysArray[RandomValue]);
+}
+
+dayWrite();
