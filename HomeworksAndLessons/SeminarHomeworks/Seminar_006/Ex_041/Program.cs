09f5259577b15f693dd701385faed2b97a266b2c
ViktorShipitsin <shipitsin.viktor.vladimirovych@yandex.ru>
2022-08-28 16:27:07 +0800

    Homework complete. Ex_041 has been completed. Ex_043 has been completed.

---
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_006/Ex_041/Program.cs
@@ -0,0 +1,20 @@
+﻿// Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
+
+
+void Main()
+{
+    double m = 0;
+    double temporary = 0;
+    int count = 0;
+    Console.Write("Сколько чисел вы хотите ввести? ");
+    m = int.Parse(Console.ReadLine());
+    for (int i = 0; i < m; i++)
+    {
+        Console.Write($"Введите число {i + 1} (из {m}):");
+        temporary = int.Parse(Console.ReadLine());
+        if (temporary > 0) {count++;}
+    }
+    Console.Write($"Введено чисел больше 0:  ({count})");
+}
+
+Main();
---
+++ HomeworksAndLessons/SeminarHomeworks/Seminar_006/Ex_043/Program.cs
@@ -0,0 +1,51 @@
+﻿// Напишите программу, которая найдёт точку пересечения двух прямых, заданных уравнениями
+// y = k1 * x + b1, y = k2 * x + b2;
+// значения b1, k1, b2 и k2 задаются пользователем.
+
+double ArgumentEnter()
+{
+    double temporary = int.Parse(Console.ReadLine());
+    Console.WriteLine();
+    return temporary;
+}
+
+double[] FunctionImplement(double[] temparray)
+{
+    double b1 = temparray[0];
+    double k1 = temparray[1];
+    double b2 = temparray[2];
+    double k2 = temparray[3];
+    
+    double x = 0;
+    x = ((-1 * b1) / (-1 * k1)) + ((-1 * b1) % (-1 * k1));
+    x = x * -1;
+    double y = k2 * x + b2;
+
+    double[] res = new double[2];
+        res[0] = x; res[1] = y;
+
+    return res;
+}
+
+void PrintResult(double[] res)
+{
+    Console.Write($"Координаты точки пересечения отрезков: ({res[0]}; {res[1]})");
+}
+
+void Main()
+{
+    double[] temparray = new double[4];
+    string[] str = {"b1", "k1", "b2", "k2"};
+    
+    for (int i = 0; i < temparray.Length; i++)
+    {
+        Console.Write($"Введите переменную {str[i]}: ");
+        temparray[i] = ArgumentEnter();
+    }
+
+    double[] res = FunctionImplement(temparray);
+
+    PrintResult(res);
+}
+
+Main();
